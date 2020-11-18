61d8497769d557eca34458032b9d92e19fe796c5
iainm342 <ism342@gmail.com>
2020-11-18 15:43:33 +0000

    added JS to main.js to make modal panels work on reading.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,51 @@
 
-const modal = document.getElementById("myModal");
+// const modal = document.getElementById("myModal");
 
 
-const btn = document.getElementById("myBtn");
+// const btn = document.getElementById("myBtn");
 
-const span = document.getElementsByClassName("close")[0];
+// const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target === modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+var btn = document.querySelectorAll("button.modal-button");
+
+var modals = document.querySelectorAll(".modal");
+
+var spans = document.getElementsByClassName("close");
+
+for (var i = 0; i < btn.length; i++) {
+    btn[i].onclick = function(e) {
+        e.preventDefault();
+        modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+for (var i = 0; i < spans.length; i++) {
+    spans[i].onclick = function () {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+        }
+    }
 }
 
 window.onclick = function(event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
+    if (event.target.classLisat.contains('modal')) {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') this.modals[index].style.display = "none";
+        }
+    }
 }
