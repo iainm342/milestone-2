963637012e1284286dab23cf639bb4fb6ccb96d3
iainm342 <ism342@gmail.com>
2020-11-20 06:14:25 +0000

    added modal body id's on reading.html, added js to push obj info to modal's on reading.html

--- scripts/main.js
+++ scripts/main.js
@@ -71,6 +71,40 @@ console.log(rest9);
 let rest10 = new readRest("Zerodegrees", "9 Bridge Street, Reading, RG1 2LR", "011 959 7959", "https://www.zerodegrees.co.uk/", "Bar", "££");
 console.log(rest10);
 
+//rest1
+let restName1 = `${rest1.name}`;
+let elVenueName1 = document.getElementById("name1");
+elVenueName1.textContent = restName1;
+
+let restAddress1 = `${rest1.address}`;
+let elVenueAddress1 = document.getElementById("address1");
+elVenueAddress1.textContent = restAddress1;
+
+let restPhone1 = `${rest1.phone}`;
+let elVenuePhone1 = document.getElementById("phone1");
+elVenuePhone1.textContent = restPhone1;
+
+let restWeb1 = `${rest1.website}`;
+let elVenueWeb1 = document.getElementById("web1");
+elVenueWeb1.textContent = restWeb1;
+
+let restType1 = `${rest1.type}`;
+let elVenueType1 = document.getElementById("type1");
+elVenueType1.textContent = restType1;
+
+let restCost1 = `${rest1.cost}`;
+let elVenueCost1 = document.getElementById("cost1");
+elVenueCost1.textContent = restCost1;
+
+// function restInfo() {
+//     for (let i=1; i <=10; i++) {
+//     let restName[i] = `${rest[i].name}`;
+//     let elVenueName[i] = document.getElementById("name[i]");
+//     elVenueName[i].textContent = restName[i];
+//     }
+// }
+
+//rest2
 let restName2 = `${rest2.name}`;
 let elVenueName2 = document.getElementById("name2");
 elVenueName2.textContent = restName2;
@@ -95,3 +129,202 @@ let restCost2 = `${rest2.cost}`;
 let elVenueCost2 = document.getElementById("cost2");
 elVenueCost2.textContent = restCost2;
 
+//rest3
+let restName3 = `${rest3.name}`;
+let elVenueName3 = document.getElementById("name3");
+elVenueName3.textContent = restName3;
+
+let restAddress3 = `${rest3.address}`;
+let elVenueAddress3 = document.getElementById("address3");
+elVenueAddress3.textContent = restAddress3;
+
+let restPhone3 = `${rest3.phone}`;
+let elVenuePhone3 = document.getElementById("phone3");
+elVenuePhone3.textContent = restPhone3;
+
+let restWeb3 = `${rest3.website}`;
+let elVenueWeb3 = document.getElementById("web3");
+elVenueWeb3.textContent = restWeb3;
+
+let restType3 = `${rest3.type}`;
+let elVenueType3 = document.getElementById("type3");
+elVenueType3.textContent = restType3;
+
+let restCost3 = `${rest3.cost}`;
+let elVenueCost3 = document.getElementById("cost3");
+elVenueCost3.textContent = restCost3;
+
+//rest4
+let restName4 = `${rest4.name}`;
+let elVenueName4 = document.getElementById("name4");
+elVenueName4.textContent = restName4;
+
+let restAddress4 = `${rest4.address}`;
+let elVenueAddress4 = document.getElementById("address4");
+elVenueAddress4.textContent = restAddress4;
+
+let restPhone4 = `${rest4.phone}`;
+let elVenuePhone4 = document.getElementById("phone4");
+elVenuePhone4.textContent = restPhone4;
+
+let restWeb4 = `${rest4.website}`;
+let elVenueWeb4 = document.getElementById("web4");
+elVenueWeb4.textContent = restWeb4;
+
+let restType4 = `${rest4.type}`;
+let elVenueType4 = document.getElementById("type4");
+elVenueType4.textContent = restType4;
+
+let restCost4 = `${rest4.cost}`;
+let elVenueCost4 = document.getElementById("cost4");
+elVenueCost4.textContent = restCost4;
+
+//rest5
+let restName5 = `${rest5.name}`;
+let elVenueName5 = document.getElementById("name5");
+elVenueName5.textContent = restName5;
+
+let restAddress5 = `${rest5.address}`;
+let elVenueAddress5 = document.getElementById("address5");
+elVenueAddress5.textContent = restAddress5;
+
+let restPhone5 = `${rest5.phone}`;
+let elVenuePhone5 = document.getElementById("phone5");
+elVenuePhone5.textContent = restPhone5;
+
+let restWeb5 = `${rest5.website}`;
+let elVenueWeb5 = document.getElementById("web5");
+elVenueWeb5.textContent = restWeb5;
+
+let restType5 = `${rest5.type}`;
+let elVenueType5 = document.getElementById("type5");
+elVenueType5.textContent = restType5;
+
+let restCost5 = `${rest5.cost}`;
+let elVenueCost5 = document.getElementById("cost5");
+elVenueCost5.textContent = restCost5;
+
+//rest6
+let restName6 = `${rest6.name}`;
+let elVenueName6 = document.getElementById("name6");
+elVenueName6.textContent = restName6;
+
+let restAddress6 = `${rest6.address}`;
+let elVenueAddress6 = document.getElementById("address6");
+elVenueAddress6.textContent = restAddress6;
+
+let restPhone6 = `${rest6.phone}`;
+let elVenuePhone6 = document.getElementById("phone6");
+elVenuePhone6.textContent = restPhone6;
+
+let restWeb6 = `${rest6.website}`;
+let elVenueWeb6 = document.getElementById("web6");
+elVenueWeb6.textContent = restWeb6;
+
+let restType6 = `${rest6.type}`;
+let elVenueType6 = document.getElementById("type6");
+elVenueType6.textContent = restType6;
+
+let restCost6 = `${rest6.cost}`;
+let elVenueCost6 = document.getElementById("cost6");
+elVenueCost6.textContent = restCost6;
+
+//rest7
+let restName7 = `${rest7.name}`;
+let elVenueName7 = document.getElementById("name7");
+elVenueName7.textContent = restName7;
+
+let restAddress7 = `${rest7.address}`;
+let elVenueAddress7 = document.getElementById("address7");
+elVenueAddress7.textContent = restAddress7;
+
+let restPhone7 = `${rest7.phone}`;
+let elVenuePhone7 = document.getElementById("phone7");
+elVenuePhone7.textContent = restPhone7;
+
+let restWeb7 = `${rest7.website}`;
+let elVenueWeb7 = document.getElementById("web7");
+elVenueWeb7.textContent = restWeb7;
+
+let restType7 = `${rest7.type}`;
+let elVenueType7 = document.getElementById("type7");
+elVenueType7.textContent = restType7;
+
+let restCost7 = `${rest7.cost}`;
+let elVenueCost7 = document.getElementById("cost7");
+elVenueCost7.textContent = restCost7;
+
+//rest8
+let restName8 = `${rest8.name}`;
+let elVenueName8 = document.getElementById("name8");
+elVenueName8.textContent = restName8;
+
+let restAddress8 = `${rest8.address}`;
+let elVenueAddress8 = document.getElementById("address8");
+elVenueAddress8.textContent = restAddress8;
+
+let restPhone8 = `${rest8.phone}`;
+let elVenuePhone8 = document.getElementById("phone8");
+elVenuePhone8.textContent = restPhone8;
+
+let restWeb8 = `${rest8.website}`;
+let elVenueWeb8 = document.getElementById("web8");
+elVenueWeb8.textContent = restWeb8;
+
+let restType8 = `${rest8.type}`;
+let elVenueType8 = document.getElementById("type8");
+elVenueType8.textContent = restType8;
+
+let restCost8 = `${rest8.cost}`;
+let elVenueCost8 = document.getElementById("cost8");
+elVenueCost8.textContent = restCost8;
+
+//rest9
+let restName9 = `${rest9.name}`;
+let elVenueName9 = document.getElementById("name9");
+elVenueName9.textContent = restName9;
+
+let restAddress9 = `${rest9.address}`;
+let elVenueAddress9 = document.getElementById("address9");
+elVenueAddress9.textContent = restAddress9;
+
+let restPhone9 = `${rest9.phone}`;
+let elVenuePhone9 = document.getElementById("phone9");
+elVenuePhone9.textContent = restPhone9;
+
+let restWeb9 = `${rest9.website}`;
+let elVenueWeb9 = document.getElementById("web9");
+elVenueWeb9.textContent = restWeb9;
+
+let restType9 = `${rest9.type}`;
+let elVenueType9 = document.getElementById("type9");
+elVenueType9.textContent = restType9;
+
+let restCost9 = `${rest9.cost}`;
+let elVenueCost9 = document.getElementById("cost9");
+elVenueCost9.textContent = restCost9;
+
+//rest10
+let restName10 = `${rest10.name}`;
+let elVenueName10 = document.getElementById("name10");
+elVenueName10.textContent = restName10;
+
+let restAddress10 = `${rest10.address}`;
+let elVenueAddress10 = document.getElementById("address10");
+elVenueAddress10.textContent = restAddress10;
+
+let restPhone10 = `${rest10.phone}`;
+let elVenuePhone10 = document.getElementById("phone10");
+elVenuePhone10.textContent = restPhone10;
+
+let restWeb10 = `${rest10.website}`;
+let elVenueWeb10 = document.getElementById("web10");
+elVenueWeb10.textContent = restWeb10;
+
+let restType10 = `${rest10.type}`;
+let elVenueType10 = document.getElementById("type10");
+elVenueType10.textContent = restType10;
+
+let restCost10 = `${rest10.cost}`;
+let elVenueCost10 = document.getElementById("cost10");
+elVenueCost10.textContent = restCost10;
