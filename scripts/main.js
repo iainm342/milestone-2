31f6f77f1c6daf298980e6807946d123e8bcb74c
iainm342 <ism342@gmail.com>
2020-11-21 09:10:28 +0000

    added first map to reading.html, basic styles and LngLat

--- scripts/main.js
+++ scripts/main.js
@@ -42,34 +42,24 @@ class readRest {
 }
 
 let rest1 = new readRest("Bill's Restaurant", "St Mary's Church House, Chain Street, Reading, RG1 2HX", "0118 939 1365", "https://www.bills-website.co.uk/restaurants/reading", "International", "££");
-console.log(rest1);
 
 let rest2 = new readRest("Brewdog", "11 Castle Street, Reading, RG1 7SB", "0118 956 8755", "https://www.brewdog.com/uk/bars/uk/brewdog-reading", "Bar", "££");
-console.log(rest2);
 
 let rest3 = new readRest("The Bull Inn", "High Street, Sonning-on-Thames, Reading, RG4 6UP", "0118 969 3901", "https://www.fullers.co.uk/hotels", "Traditional", "£");
-console.log(rest3);
 
 let rest4 = new readRest("Carluccio's", "Forbury Square, Reading, RG1 3EY", "0118 958 3095", "https://www.carluccios.com/restaurants/reading/", "Italian", "£");
-console.log(rest4);
 
 let rest5 = new readRest("Coppa Club", "The Great House, Sonning-on-Thames, Reading, RG4 6UT", "0118 921 9890", "https://coppaclub.co.uk/sonning/", "Italian", "££");
-console.log(rest5);
 
 let rest6 = new readRest("London Street Brasserie", "The Oracle, 2-4 London Street, Reading, RG1 4PN", "0118 950 5036", "https://www.londonstbrasserie.co.uk/", "Modern British", "£££");
-console.log(rest6);
 
 let rest7 = new readRest("L'Ortolan", "Church Lane, Shinfield, Reading, RG2 9BY", "0118 988 8500", "https://lortolan.com/", "French", "££££");
-console.log(rest7);
 
 let rest8 = new readRest("Malmaison", "Great Western House, 18-20 Station Road, Reading, RG1 1JX", "0844 693 0660 ", "https://www.malmaison.com/locations/reading/", "French", "£££");
-console.log(rest8);
 
 let rest9 = new readRest("The Real Greek", "The Oracle, Reading, RG1 2AT", "0118 995 2270", "https://www.therealgreek.com/", "Greek", "££");
-console.log(rest9);
 
 let rest10 = new readRest("Zerodegrees", "9 Bridge Street, Reading, RG1 2LR", "011 959 7959", "https://www.zerodegrees.co.uk/", "Bar", "££");
-console.log(rest10);
 
 //rest1
 let restName1 = `${rest1.name}`;
---
+++ scripts/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 13,
+                center: {
+                    lat: 51.45625,
+                    lng: -0.97113
+                }
+            });
+        }
