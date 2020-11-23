0f158fc104cb553b89965ca98a2c8249d02a6d0a
iainm342 <ism342@gmail.com>
2020-11-23 05:32:34 +0000

    added second part of function to filter.js - needs reviewing

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,18 +1,19 @@
 (function () {
   var $imgs = $("#gallery img");
-  var $buttons = $("#buttons");
+  var $buttons1 = $("#buttons1");   
+var $buttons2 = $("#buttons2");
   var tagged = {};
 
   $imgs.each(function () {
     var img = this;
-    var tags = $(this).data("tags");
+    var tags1 = $(this).data("tags1");
 
-    if (tags) {
-      tags.split(",").forEach(function (tagName) {
-        if (tagged[tagName] == null) {
-          tagged[tagName] = [];
+    if (tags1) {
+      tags1.split(",").forEach(function (tagName1) {
+        if (tagged[tagName1] == null) {
+          tagged[tagName1] = [];
         }
-        tagged[tagName].push(img);
+        tagged[tagName1].push(img);
       });
     }
   });
@@ -24,16 +25,55 @@
       $(this).addClass("active").siblings().removeClass("active");
       $imgs.show();
     },
-  }).appendTo($buttons);
+  }).appendTo($buttons1);
 
-  $.each(tagged, function (tagName) {
+  $.each(tagged, function (tagName1) {
     $("<button/>", {
-      text: tagName + " (" + tagged[tagName].length + ")",
+      text: tagName1 + " (" + tagged[tagName1].length + ")",
       click: function () {
         $(this).addClass("active").siblings().removeClass("active");
-        $imgs.hide().filter(tagged[tagName]).show();
+        $imgs.hide().filter(tagged[tagName1]).show();
       },
-    }).appendTo($buttons);
+    }).appendTo($buttons1);
+  });
+  $("button").css({
+    "background-color": "red",
+    color: "white",
+    "border-radius": "15px",
+    "margin": "10px"
+  });
+
+  $imgs.each(function () {
+    var img = this;
+    var tags2 = $(this).data("tags2");
+
+    if (tags2) {
+      tags2.split(",").forEach(function (tagName2) {
+        if (tagged[tagName2] == null) {
+          tagged[tagName2] = [];
+        }
+        tagged[tagName2].push(img);
+      });
+    }
+  });
+  console.log(tagged);
+  $("<button/>", {
+    text: "Show All",
+    class: "active",
+    click: function () {
+      $(this).addClass("active").siblings().removeClass("active");
+      $imgs.show();
+    },
+  }).appendTo($buttons2);
+
+  $.each(tagged, function (tagName2) {
+    $("<button/>", {
+      text: tagName2 + " (" + tagged[tagName2].length + ")",
+      click: function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $imgs.hide().filter(tagged[tagName2]).show();
+      },
+    }).appendTo($buttons2);
   });
   $("button").css({
     "background-color": "red",
@@ -42,3 +82,4 @@
     "margin": "10px"
   });
 })();
+
