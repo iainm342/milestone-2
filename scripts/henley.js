2e8e83c4134cc39cff8ed8d9533383033d0866b5
iainm342 <ism342@gmail.com>
2020-12-02 10:02:04 +0000

    removed commented out code from pages

--- scripts/henley.js
+++ scripts/henley.js
@@ -1,35 +1,5 @@
 
 
-// var btn = document.querySelectorAll("button.modal-button");
-
-// var modals = document.querySelectorAll(".modal");
-
-// var spans = document.getElementsByClassName("close");
-
-// for (var i = 0; i < btn.length; i++) {
-//     btn[i].onclick = function(e) {
-//         e.preventDefault();
-//         modal = document.querySelector(e.target.getAttribute("href"));
-//         modal.style.display = "block";
-//     }
-// }
-
-// for (var i = 0; i < spans.length; i++) {
-//     spans[i].onclick = function () {
-//         for (var index in modals) {
-//             if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-//         }
-//     }
-// }
-
-// window.onclick = function(event) {
-//     if (event.target.classList.contains('modal')) {
-//         for (var index in modals) {
-//             if (typeof modals[index].style !== 'undefined') this.modals[index].style.display = "none";
-//         }
-//     }
-// }
-
 //Accordion
 
 var acc = document.getElementsByClassName("accordion");
